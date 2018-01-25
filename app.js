7b3811e0644911bf62a208fd4875198abdb7cbfc
Guillermo <Guillermo@xite-mem-mac.local>
2018-01-25 12:42:53 +0100

    new feature goes live

--- app.js
+++ app.js
@@ -3,4 +3,6 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.get('/hola', (req, res) => res.send('Just an Hola endpoint'))
+
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
